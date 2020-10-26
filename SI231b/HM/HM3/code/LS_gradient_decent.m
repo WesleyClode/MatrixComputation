704ba7a2dc6c82fb1f0a185b5d671a5b2a786ead
Yun Tian <tianyunchn@outlook.com>
2020-10-26 11:20:50 +0800

    Update LS_gradient_decent.m

--- SI231b/HM/HM3/code/LS_gradient_decent.m
+++ SI231b/HM/HM3/code/LS_gradient_decent.m
@@ -5,36 +5,36 @@ headerlinesIn = 0;
 A = importdata(file1,delimiterIn,headerlinesIn);
 Y = importdata(file2,delimiterIn,headerlinesIn);
 
-alpha = 1e-1;
-eps = 1e-5;
+alpha = 1e-5;
+eps = 1e-2;
 
 data_len = size(Y, 1);
 var_len = size(A, 2);
-sse = 0; sse_new = 0;
+
+J = 0; J_new = 0;
+
 x = zeros(var_len,1);
 grad_x = zeros(var_len,1);
+
 count = 0;
+
 for step = 1:1000000
+    
     count = count + 1;
-    for j = 1:data_len
-        base = A(j,:)*x - Y(j);
-        grad_x = (A(j,:).*base)';
-    end
-        grad_x = grad_x/data_len;
+    
+    grad_x = alpha * A'*(A*x-Y);
         
-        x = x - alpha .* grad_x;
+    x = x - alpha * grad_x;
+    
+    J_new = (A*x-Y)'*(A*x-Y)/2
+    
+    
+    if abs(J_new - J)<eps
         
-        for k = 1:data_len
-            sse_new = sse_new + power((A(k,:)*x-Y(k)),2);
-        end
-        sse - sse_new
-    if abs(sse_new - sse)<eps
         break
+        
     else
-        sse = sse_new;
+        J = J_new;
     end
     
-    if abs(sse_new - sse)<eps
-        break
-    end
 end
